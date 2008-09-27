a2f282d749b922079b550bed04e0a17c49a4fd83
Konstantin Merenkov <kmerenkov@gmail.com>
2008-09-27 20:20:35 +0400

    - Changed license from beerware to WTFPL - Renamed sample test case #1

--- tests.py
+++ tests.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <kmerenkov@gmail.com> wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2008 Konstantin Merenkov <kmerenkov@gmail.com>
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 
 import unittest
 import re
@@ -283,7 +292,7 @@ class JobRelatedTestCase(unittest.TestCase):
 
 
 class SamplesTestCase(unittest.TestCase):
-    def test_001(self):
+    def test_integer_list_001(self):
         """ test case for sample #1 """
         l = [1,2,3,4,5,6]
         ref_struct = v.List([v.Int()])
