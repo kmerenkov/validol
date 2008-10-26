5dba57435f41a4c5fb9d7720a197fea1b982ffd7
Konstantin Merenkov <kmerenkov@gmail.com>
2008-10-26 12:10:47 +0300

    Added load_tests for lists and tuples.

--- validol/load_tests.py
+++ validol/load_tests.py
@@ -19,26 +19,50 @@ from __init__ import validate, AnyOf, Many, Optional, Scheme, BaseValidator
 
 
 class DictTestCase(unittest.TestCase):
-    def test_hash_good_001(self):
+    def test_good_001(self):
         x = dict(map(lambda x: (str(x), x), xrange(1000)))
         s = dict(map(lambda x: (str(x), int), xrange(1000)))
         self.assertTrue(validate(s, x))
 
-    def test_hash_bad_001(self):
+    def test_bad_001(self):
         x = dict(map(lambda x: (str(x), x), xrange(1000)))
         s = dict(map(lambda x: (str(x), int), xrange(1000)))
         s['999'] = str
         self.assertFalse(validate(s, x))
 
-    def test_hash_good_002(self):
+    def test_good_002(self):
         x = dict(map(lambda x: (str(x), x), xrange(1000)))
         s = dict(map(lambda x: (Optional(str(x)), int), xrange(1000)))
         self.assertTrue(validate(s, x))
 
-    def test_hash_bad_002(self):
+    def test_bad_002(self):
         x = dict(map(lambda x: (str(x), x), xrange(1000)))
         s = dict(map(lambda x: (Optional(str(x)), int), xrange(1000)))
         s['999'] = str
+        self.assertFalse(validate(s, x))
+
+
+class ListTestCase(unittest.TestCase):
+    def test_good_001(self):
+        x = [ str(x) for x in xrange(1000) ]
+        s = [str]
+        self.assertTrue(validate(s, x))
+
+    def test_bad_001(self):
+        x = [ str(x) for x in xrange(1000) ]
+        s = [int]
+        self.assertFalse(validate(s, x))
+
+
+class TupleTestCase(unittest.TestCase):
+    def test_good_001(self):
+        x = tuple(map(str, xrange(10000)))
+        s = tuple(map(str, xrange(10000)))
+        self.assertTrue(validate(s, x))
+
+    def test_bad_001(self):
+        x = tuple(map(str, xrange(10000)))
+        s = tuple(map(str, xrange(9999)) + [10000])
         self.assertFalse(validate(s, x))
 
 
